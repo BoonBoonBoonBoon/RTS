19076be19ec6722cd6ad73c4b92c00ee1759be12
BoonBoonBoonBoon <Samacoitselliot@gmail.com>
2023-12-02 12:07:54 +0000

    push

---
+++ Source/RTS/Public/UserController.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "UserController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RTS_API AUserController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
