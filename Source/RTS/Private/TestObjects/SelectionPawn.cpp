d523e54eccd769fc060f9134531240fbe70d470a
BoonBoonBoonBoon <Samacoitselliot@gmail.com>
2024-01-04 16:24:41 +0000

    .

--- Source/RTS/Private/TestObjects/SelectionPawn.cpp
+++ Source/RTS/Private/TestObjects/SelectionPawn.cpp
@@ -21,6 +21,8 @@ void ASelectionPawn::NotifyHit(UPrimitiveComponent* MyComp, AActor* Other, UPrim
 void ASelectionPawn::OverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	// Do something when hit
+	UE_LOG(LogTemp, Warning, TEXT("Actor %s was hit!"), *GetName());
 }
 
 void ASelectionPawn::OverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
